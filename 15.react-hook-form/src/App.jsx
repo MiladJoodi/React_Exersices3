f863e26448e65147273ce00d1a805d42b3ea7c2c
Milad Joodi <winbeta.com@gmail.com>
2024-01-05 23:04:17 +0330

    ♥

--- 15.react-hook-form/src/App.jsx
+++ 15.react-hook-form/src/App.jsx
@@ -8,20 +8,20 @@ export default function App() {
     defaultValues: {
       name: "",
       email: "",
-    },
-  });
+    }
+  })
 
-  const formSubmitting = (data) => {
-    console.log("Data =>", data);
-  };
+  const formSubmiting = (data)=>{
+    console.log("Data=> ", data)
+  }
 
   return (
     <div className="login-page">
       <div className="form">
-        <form className="login-form" onSubmit={handleSubmit(formSubmitting)}>
-          <input type="text" {...register("name")} placeholder="Name ..." />
+        <form className="login-form" onSubmit={handleSubmit(formSubmiting)}>
+          <input type="text" {...register('name')} placeholder="Name ..." />
 
-          <input type="text" {...register("email")} placeholder="Email ..." />
+          <input type="text" {...register('email')} placeholder="Email ..." />
 
           <button type="submit">Register</button>
           <p className="message">
