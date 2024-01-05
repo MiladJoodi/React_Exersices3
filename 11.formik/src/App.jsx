95713c9dc662f8e3b37dbc91c5936c9383cdc4a8
Milad Joodi <winbeta.com@gmail.com>
2024-01-05 14:35:33 +0330

    formik

--- 11.formik/src/App.jsx
+++ 11.formik/src/App.jsx
@@ -4,94 +4,71 @@ import { useFormik } from "formik";
 import "./App.css";
 
 export default function App() {
-
   const form = useFormik({
     initialValues: { name: "", email: "" },
 
-    onSubmit: (values, {setSubmitting}) => {
+    onSubmit: (values, { setSubmitting }) => {
       console.log("Form Inputs Data=>", values);
-      setTimeout(()=>{
-        setSubmitting(false)
-      }, 3000)
+      setTimeout(() => {
+        setSubmitting(false);
+      }, 3000);
     },
-    validate: (values)=>{
-      const errors = {}
+    validate: (values) => {
+      const errors = {};
 
-      if(values.name === ''){
-        errors.name = 'وارد کردن نام اجباری می باشد'
-      } else if (values.name.length<4){
-        errors.name = "نام شما باید حداقل 4 کاراکتر باشد"
+      if (values.name === "") {
+        errors.name = "وارد کردن نام اجباری می باشد";
+      } else if (values.name.length < 4) {
+        errors.name = "نام شما باید حداقل 4 کاراکتر باشد";
       }
 
-      if(values.email === ''){
-        errors.email = 'وارد کردن ایمیل الزامیست'
-      }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
-        errors.email = 'ایمیل وارد شده معتبر نیست'
+      if (values.email === "") {
+        errors.email = "وارد کردن ایمیل الزامیست";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = "ایمیل وارد شده معتبر نیست";
       }
-      
 
-      return errors
-    }
-  })
+      return errors;
+    },
+  });
 
   return (
     <div className="login-page">
       <div className="form">
-        <Formik
-        validate={(values)=>{
-          const errors = {}
+        <form onSubmit={form.submitHandler}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name..."
+            value={form.values.name}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur}
+          />
+          {form.errors.name && form.touched.name && <h2>{form.errors.name}</h2>}
 
-          if(values.name === ''){
-            errors.name = 'وارد کردن نام اجباری می باشد'
-          } else if (values.name.length<4){
-            errors.name = "نام شما باید حداقل 4 کاراکتر باشد"
-          }
+          <input
+            type="email"
+            name="email"
+            placeholder="Email..."
+            value={form.values.email}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur}
+          />
+          {form.errors.email && form.touched.email && <h2>{form.errors.email}</h2>}
 
-          if(values.email === ''){
-            errors.email = 'وارد کردن ایمیل الزامیست'
-          }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
-            errors.email = 'ایمیل وارد شده معتبر نیست'
-          }
-          
-
-          return errors
-        }}
-          initialValues={{ name: "", email: "" }}
-          onSubmit={(values,{setSubmitting}) => {
-            console.log("Form Inputs Data=>", values);
-            setTimeout(()=>{
-              setSubmitting(false)
-            }, 3000)
-          }}
-        >
-          {({ isSubmitting }) => (
-              <Form className="login-form">
-                {/* {console.log(touched)} */}
-                <Field type="text" name="name" placeholder="Name..." />
-                {/* {errors.name && touched.name && <h2>{errors.name}</h2>} */}
-                <ErrorMessage name="name" component="h1">
-                  {(msg)=> <span>{msg}</span>}
-                </ErrorMessage>
-                <Field type="email" name="email" placeholder="Email..." />
-                <ErrorMessage name="email" component="h1">
-                  {(msg)=>(
-                    <span>{msg}</span>
-                  )}
-                </ErrorMessage>
-                {/* {errors.email && touched.email && errors.email} */}
-                <button type="submit"
-                className={isSubmitting ? 'invalid-btn' : ''}
-                disabled={isSubmitting}
-                >
-                  {isSubmitting ? 'Loading': 'Register'}
-                  </button>
-                <p className="message">
-                already registered? <a href="">Sign In</a>
-              </p>
-              </Form>
-              
-          )}
-        </Formik>
+          <button
+            type="submit"
+            className={form.isSubmitting ? "invalid-btn" : ""}
+            disabled={form.isSubmitting}
+          >
+            {form.isSubmitting ? "Loading" : "Register"}
+          </button>
+          <p className="message">
+            already registered? <a href="">Sign In</a>
+          </p>
+        </form>
       </div>
     </div>
   );
