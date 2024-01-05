db2a435b8246e7ecf0d06ec1d48a7114052fe5ac
Milad Joodi <winbeta.com@gmail.com>
2024-01-05 21:26:33 +0330

    as

--- 13.Yup/src/Validations/register.js
+++ 13.Yup/src/Validations/register.js
@@ -4,7 +4,7 @@ const registerSchema = Yup.object().shape({
     name: Yup.string()
     .min(3, "اسم حداقل باید 3 کارکتر باشد")
     .max(12, 'اسم حداکثر باید 12 کاراکتر باشد')
-    .required(),
+    .required("لطفا نام خود را وارد کنید"),
     email: Yup.string()
     .email()
     .min(10, "ایمیل می تواند حداکثر 30 کاراکتر باشد")
