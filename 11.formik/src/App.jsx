498361422301730401ed0373688c330644aae1ca
Milad Joodi <winbeta.com@gmail.com>
2024-01-05 15:14:21 +0330

    as

--- 11.formik/src/App.jsx
+++ 11.formik/src/App.jsx
@@ -12,12 +12,12 @@ export default function App() {
       email: "",
       password: "",
       desc: "",
-      product: "-1",
-      check: false,
+      products: "-1",
+      check: false
     },
-    onSubmit: (values) => {
+    onSubmit: (values)=>{
       console.log(values);
-    },
+    }
   });
 
   return (
@@ -25,75 +25,26 @@ export default function App() {
       <p className="form-title">لطفا فرم زیر را کامل کنید</p>
 
       <main>
-        <input
-          type="text"
-          name="phone"
-          onChange={form.handleChange}
-          value={form.values.phone}
-          onBlur={form.handleBlur}
-          placeholder="شماره تماس"
-        />
-        <input
-          type="text"
-          name="name"
-          onChange={form.handleChange}
-          value={form.values.name}
-          onBlur={form.handleBlur}
-          placeholder="نام و نام خانوادگی"
-        />
-
-        <input
-          type="text"
-          name="address"
-          onChange={form.handleChange}
-          value={form.values.address}
-          onBlur={form.handleBlur}
-          placeholder="ادرس"
-        />
-
-        <input
-          type="email"
-          name="email"
-          onChange={form.handleChange}
-          value={form.values.email}
-          onBlur={form.handleBlur}
-          placeholder="ایمیل"
-        />
-        <input
-          type="password"
-          name="password"
-          onChange={form.handleChange}
-          value={form.values.password}
-          onBlur={form.handleBlur}
-          placeholder="رمز عبور"
-        />
-        <input
-          type="text"
-          name="desc"
-          onChange={form.handleChange}
-          value={form.values.desc}
-          onBlur={form.handleBlur}
-          placeholder="توضیحات"
-        />
+        <input type="text" name="phone" onChange={form.handleChange} placeholder="شماره تماس" />
+        <input type="text" name="name" onChange={form.handleChange} placeholder="نام ونام خانوادگی" />
+        <input type="text" name="address" onChange={form.handleChange} placeholder="آدرس" />
+        <input type="email" name="email" onChange={form.handleChange} placeholder="ایمیل" />
+        <input type="password" name="password" onChange={form.handleChange} placeholder="رمز عبور" />
+        <input type="text" name="desc" onChange={form.handleChange} placeholder="توضیحات" />
       </main>
 
       <section>
-        <select
-          name="product"
-          onChange={form.handleChange}
-          id="selectBox"
-          defaultValue="-1"
-        >
-          <option value="-1">محصول خود را انتخاب کنید</option>
-          <option value="kif">کیف</option>
-          <option value="kafsh">کفش</option>
-          <option value="kamarband">کمربند</option>
+        <select name="product" id="selectBox" defaultValue="-1">
+          <option value='-1'>محصول خود را انتخاب کنید</option>
+          <option value='kif'>کیف</option>
+          <option value='kafsh'>کفش</option>
+          <option value='kamarband'>کمربند</option>
         </select>
         <input
           className="form-check-input"
           type="checkbox"
           id="flexCheckDefault"
-          checked={form.values.check}
+          checked= {form.values.check}
           name="check"
           onChange={form.handleChange}
         />
