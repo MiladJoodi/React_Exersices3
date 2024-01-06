2e3747111563986e4293b42c657889b39508cb79
Milad Joodi <winbeta.com@gmail.com>
2024-01-06 17:46:24 +0330

    stopr

--- 15.react-hook-form/src/App.jsx
+++ 15.react-hook-form/src/App.jsx
@@ -1,86 +1,63 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { DevTool } from '@hookform/devtools'
+import { DevTool } from "@hookform/devtools";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 import "./App.css";
 
 export default function App() {
-  const { 
+  const schema = yup.object({
+    name: yup.string().required("وارد کردن فیلد نام اجباریه !!"),
+    email: yup
+      .string()
+      .email("ایمیل وارد شده معتبر نمی‌باشد !!")
+      .required("وارد کردن فیلد ایمیل اجباریه !!"),
+  });
+
+  const {
     register,
     control,
+    getValues,
     handleSubmit,
-    formState: { errors } } = useForm({
-    defaultValues: async ()=>{
-      const res = await fetch('https://jsonplaceholder.typicode.com/users/1')
-      const data = await res.json()
-      // این دیتا کل اون اطلاعاتی هستش که از فتچ ککردن میاد
-      return{
+    formState: { errors },
+  } = useForm({
+    defaultValues: async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+      const data = await res.json();
+
+      return {
         name: data.name,
         email: data.email,
-        socials: {
-          telegram: '@Joodi',
-          instagram: '@Milad_Joodi'
-        }
-      }
-    }
-  })
+      };
+    },
+    resolver: yupResolver(schema),
+  });
 
-  console.log("error=>", errors)
+  console.log("Errors =>", errors);
 
-  const formSubmiting = (data) => {
-    console.log("Data=> ", data)
-  }
+  const formSubmitting = (data) => {
+    // console.log("Data =>", data);
+    console.log("Form Values =>", getValues(["name", "email"]));
+  };
 
   return (
     <div className="login-page">
       <div className="form">
-        <form className="login-form" onSubmit={handleSubmit(formSubmiting)}>
-          <input type="text" {...register('name', {
-            required: "وارد کردن نام الزامیست",
-            minLength: {
-              value: 3,
-              message: "حداقل 3 کاراکتر وارد کنید"
-            },
-            maxLength: {
-              value: 12,
-              message: "حداکثر 12 کاراکتر وارد کنید"
-            }
-          })}
-            placeholder="Name ..." />
+        <form className="login-form" onSubmit={handleSubmit(formSubmitting)}>
+          <input type="text" {...register("name")} placeholder="Name ..." />
           {errors.name && errors.name.message}
 
-          <input type="text" {...register('email', {
-            required: "وارد کردن ایمیل الزامیست",
-            minLength: {
-              value: 10,
-              message: "حداقل 10 کاراکتر وارد کنید"
-            },
-            maxLength: {
-              value: 30,
-              message: "حداکثر 30 کاراکتر وارد کنید"
-            },
-            pattern: {
-              value: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/g,
-              message: "فرمت ایمیل وارد شده اشتباه است"
-            }
-          })} placeholder="Email ..." />
+          <input type="text" {...register("email")} placeholder="Email ..." />
           {errors.email && errors.email.message}
-
-          <input type="text" {...register('socials.telegram', {
-            
-          })} placeholder="Telegram ..." />
-
-          <input type="text" {...register('socials.instagram', {
-            
-          })} placeholder="Instagram ..." />
 
           <button type="submit">Register</button>
           <p className="message">
             Already registered? <a href="#">Sign In</a>
           </p>
         </form>
-        <DevTool control={control} />
       </div>
+      <DevTool control={control} />
     </div>
   );
 }
