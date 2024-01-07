e53dc9394ce25ab4c9e90025b369bd1d75f6a68c
Milad Joodi <winbeta.com@gmail.com>
2024-01-07 18:15:18 +0330

    qw

---
+++ 18-2/src/i18n/en.js
@@ -0,0 +1,19 @@
+const en = {
+    registerForm: "Registeration Form",
+    alreadyRegister: "already registered",
+    login: ' Login',
+    name: 'Name',
+    namePlaceHolder: 'Enter Your Name',
+    email: 'Email',
+    emailPlaceHolder: 'Enter Your Email',
+    password: 'Password',
+    passwordPlaceHolder: 'Enter Your Password',
+    terms: 'I read and agree to the terms and conditions',
+    registerBtn: 'Register',
+    gmailBtn: 'Login With Gmail',
+    facebookBtn: 'Login With Facebook',
+    oneStep: 'You are one step away from membership',
+    confirm: 'After confirming the membership, an email with a confirmation link will be sent to you.'
+
+}
+export default en;
---
+++ 18-2/src/i18n/fa.js
@@ -0,0 +1,18 @@
+const fa = {
+    registerForm: "فرم عضویت",
+    alreadyRegister: "قبلا عضو شده اید؟",
+    login: 'ورود',
+    name: 'نام',
+    namePlaceHolder: 'نام خود را وارد کنید',
+    email: 'ایمیل',
+    emailPlaceHolder: 'ایمیل خود را وارد کنید',
+    password: 'رمز عبور',
+    passwordPlaceHolder: 'رمز عبور خود را وارد کنید',
+    terms: 'قوانین را خوانده و موافقم',
+    registerBtn: 'عضویت',
+    gmailBtn: 'ورود با جیمیل',
+    facebookBtn: 'ورود با فیسبوک',
+    oneStep: 'یک قدم تا عضویت فاصله دارید',
+    confirm: 'پس از عضویت، یک ایمیل حاوی لینک تایید برایتان ارسال خواهد شد.'
+}
+export default fa;
