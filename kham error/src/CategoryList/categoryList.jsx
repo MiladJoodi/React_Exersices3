86ed22ac361cfa674b8d0a4d80667aebea70a9be
Milad Joodi <winbeta.com@gmail.com>
2024-02-07 18:29:38 +0330

    0

--- kham error/src/CategoryList/categoryList.jsx
+++ kham error/src/CategoryList/categoryList.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react"
+import axios from "../axios"
+
 const CategoryList = () => {
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            const response = await axios.get('/FoodCategory/categories')
+            console.log(response.data)
+
+            fetchCategories()
+        }
+    }, [])
+
     return (
-    <div>Category List</div>
-)
+        <div>Category List</div>
+    )
 }
 
 export default CategoryList
