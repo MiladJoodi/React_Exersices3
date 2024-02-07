7dcf82bd02ffed43880045e39319cfa71ec66214
Milad Joodi <winbeta.com@gmail.com>
2024-02-07 20:05:06 +0330

    loadi

--- kham error/src/CategoryList/categoryList.jsx
+++ kham error/src/CategoryList/categoryList.jsx
@@ -1,19 +1,40 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from "../axios"
 
 const CategoryList = () => {
+    const [loading, setLoading] = useState(true)
+    const [categories, setCategories] = useState([])
 
     useEffect(() => {
         const fetchCategories = async () => {
             const response = await axios.get('/FoodCategory/categories')
-            console.log(response.data)
-
-            fetchCategories()
+            setCategories(response.data)
+            setLoading(false)
         }
+        fetchCategories()
     }, [])
 
     return (
-        <div>Category List</div>
+        <nav className="container mt-n5">
+            <div className="d-flex align-items-center bg-white rounded-3 shadow-lg py-4" style={{height: "80px"}}>
+                <ul className="nav">
+                    <li className="nav-item">
+                        <a href="" className="nav-link">
+                            همه فست فود ها 
+                        </a>
+                    </li>
+                    {
+                        categories.map(category=>(
+                            <li className="nav-item" key={category.id}>
+                                <a href="" className="nav-link">
+                                    {category.name}
+                                </a>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </nav>
     )
 }
 
