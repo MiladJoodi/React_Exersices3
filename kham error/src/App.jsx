f119f978c41cba37d55419d8396b7205ab2a4841
Milad Joodi <winbeta.com@gmail.com>
2024-02-07 18:09:47 +0330

    react

---
+++ kham error/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Header/Header'
+import CategoryList from './CategoryList/categoryList'
+
+function App() {
+
+  return (
+    <div className='wrapper bg-faded-dark'>
+      <Header></Header>
+      <CategoryList />
+    </div>
+  )
+}
+
+export default App
